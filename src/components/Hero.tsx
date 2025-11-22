f36b8b9d880b212a9d8c7640a48f62b7fcc123e6
sohamchavan07 <soham777chavan777@gmail.com>
2025-11-23 00:16:54 +0530

    update linkden link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
 
   const socialLinks = [
     { icon: Github, href: "https://github.com/sohamchavan07", label: "GitHub" },
-    { icon: Linkedin, href: "https://linkedin.com/in/soham-pramod-chavan", label: "LinkedIn" },
+    { icon: Linkedin, href: "https://linkedin.com/in/sohamchavan07", label: "LinkedIn" },
     { icon: Twitter, href: "https://twitter.com/soham_chavan07", label: "X (Twitter)" },
     { icon: Mail, href: "mailto:sohamchavan.sc07@gmail.com", label: "Email" },
   ];
